3d95a2368d4848ea675d7422e692a2e9ff31111d
ophub <68696949+ophub@users.noreply.github.com>
2020-10-22 10:24:20 +0800

    Update diy-part1.sh

--- router/linksys_wrt3200acm/diy-part1.sh
+++ router/linksys_wrt3200acm/diy-part1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#=================================================
+#========================================================================================================================
 # https://github.com/ophub/op
 # Description: Automatically Build OpenWrt for Linksys WRT3200ACM
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
 # Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
 # Copyright (C) 2020 https://github.com/ophub/op
-#=================================================
+#========================================================================================================================
 
 # Uncomment a feed source
 sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
